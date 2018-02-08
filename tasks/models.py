f73256ee1017acf357dc3ce591399af0f5d0fcb5
nikolay.kurochkin <dzerdzerdzer@gmail.com>
2018-02-08 13:40:40 +1000

    updated models

--- tasks/models.py
+++ tasks/models.py
@@ -1,7 +1,25 @@
 from django.db import models
+from .utils import ChoiceEnum
+
+
+class StatusEnum(ChoiceEnum):
+    not_tested = 0
+    tested = 1
+    on_review = 2
+    done = 3
 
 
 class Task(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     file = models.FileField()
+    status = models.CharField(max_length=1, choices=StatusEnum.choices())
+
+
+class TestCase(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    file = models.FileField()
+    task = models.ForeignKey(
+        Task, verbose_name='Тесты для задач', related_name='cases',
+        on_delete=models.CASCADE)
